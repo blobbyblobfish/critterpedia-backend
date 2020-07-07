ea481eb85ecc0837d1401619724864e68fbc70e5
blobbyblobfish <aywang6@gmail.com>
2020-07-07 16:32:12 -0400

    Added delete routes and methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,40 +1,59 @@
 class UsersController < ApplicationController
     before_action :authorized, only: [:stay_logged_in]
 
+    def index
+        @users = User.all
+        render json: @users
+    end
+    
     def show
         @user = User.find(params[:id])
         render json: @user
     end
 
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        render json: @user
+    end
+
     def login
       @user = User.find_by(username: params[:username])
+
       if @user && @user.authenticate(params[:password])
         wristband = encode_token({user_id: @user.id})
         render json: {
           user: UserSerializer.new(@user),
           token: wristband
         }
+
       else
         render json: {error: "You messed up."}
       end
+
     end
 
     def stay_logged_in
       wristband = encode_token({user_id: @user.id})
+
       render json: {
         user: UserSerializer.new(@user),
         token: wristband
       }
+
     end
 
     def create
       @user = User.create(user_params)
+
       if @user.valid?
         wristband = encode_token({user_id: @user.id})
+
         render json: {
           user: UserSerializer.new(@user),
           token: wristband
         }
+
       else
         render json: {error: "A user with that username exists"}
       end
@@ -44,7 +63,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-      params.permit(:username, :password)
+      params.permit(:username, :password, :hemisphere)
     end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :user_critters
-    has_many :critters, through: user_critters
+    has_many :critters, through: :user_critters
 
     validates :username, uniqueness: true
     has_secure_password
