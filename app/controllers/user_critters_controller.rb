12c71629546dd7b9470f258df90a9c2b4bfaee4c
blobbyblobfish <aywang6@gmail.com>
2020-07-07 15:14:42 -0400

    Built out routes and controller methods

--- app/controllers/user_critters_controller.rb
+++ app/controllers/user_critters_controller.rb
@@ -1,2 +1,13 @@
 class UserCrittersController < ApplicationController
+    before_action :authorized, only: [:create]
+
+    def create
+        @user_critter = @user.user_critters.create(user_critter_params)
+        render json: @user_critter
+    end
+
+    private
+    def user_critter_params
+        params.permit(:critter_id)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
     has_many :user_critters
     has_many :critters, through: user_critters
 
-    has_secure_password
-
     validates :username, uniqueness: true
+    has_secure_password
 end
